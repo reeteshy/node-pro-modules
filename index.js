fb02c5eb4a127bb4e0f0b4adeb62b0f257a811c3
Reetesh Yadav <reeteshy15@gmailcom>
2024-04-02 10:52:09 +0530

    remove limit and update home content

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import express from "express";
 const app = express()
 const PORT = 3000;
 
-app.use(limiter)
-
 app.get('/', (req, resp) => {
-    resp.send("<h1>Hello World</h1>")
+    resp.send("<h1>Welcome to home Route</h1>")
 })
 
 app.listen(PORT, ()=>{
