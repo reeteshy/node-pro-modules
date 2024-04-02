05ffb861d3d5e70f7141113d454dc6b2ab849ae3
Reetesh Yadav <reeteshy15@gmailcom>
2024-04-02 10:54:43 +0530

    added request-limit feature

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# node-pro-modules
+## Brute-force-attack 
+- When attacker tries to login with the number of credentionl from his dicsnary up to unil and unless he got succeed to login to prevent this we are using limit the request within timeframe and use captch/Recaptcha and request throttaling
+## dos attack 
+- when attackers reqest on your server 100/1000/10000 time in a second then it makes server busy to responding when actual client send request this is called dos( Denial of service ).
+## ddos attack 
+- attcker have multiple server and system to send request 1000/10000 reuest in second to exploit or server. Then when actual client will visit the api then server will not respond to actual client due to server busy.
+
+## Rate limiting
+
+- Server naver overloaded
+- Prevent from above attacks
+- Limiting the number of request
+
+Vs code extention - REST Client
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
 const app = express()
 const PORT = 3000;
 
+app.use(limiter)
+
 app.get('/', (req, resp) => {
-    resp.send("<h1>Welcome to home Route</h1>")
+    resp.send("<h1>Hello World</h1>")
+})
+
+
+app.get('/api/v1/login', (req, resp) => {
+    resp.send("<h1>Login view page</h1>")
+})
+app.post('/api/v1/login', (req, resp) => {
+    resp.send("<h1>Post login form</h1>")
+})
+
+app.get('/api/v1/register', (req, resp) => {
+    resp.send("<h1>register view page</h1>")
+})
+app.post('/api/v1/register', (req, resp) => {
+    resp.send("<h1>Post register form</h1>")
 })
 
 app.listen(PORT, ()=>{
