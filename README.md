d31fd30b965e8b68ecdb6c3d45f46369dafe5c89
Reetesh Yadav <reeteshy15@gmailcom>
2024-04-02 11:51:44 +0530

    generate ssl and load the file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # node-pro-modules
+
+## steps to follow
+- create a server and put the two propertu key and cert
+- create a folder with cert name
+- generate private file by using below cmd
+openssl genrsa -out key.pen
+- generate the cert file using pem
+openssl req -new -key key.pem -out csr.pem
+- fill ino ( Country, company )
+- openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out cert.pem
+
+-- x509 -> standered togenerate key
+-- -days 365 -> validity of ssl certificate
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
+import path from 'path'
+import fs from 'fs'
+import https from 'https'
+import { fileURLToPath } from 'url';
+
 const app = express()
+
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
+
+
 
 app.get('/', (req, resp) => {
     resp.send("<h1>Welcome to home Route</h1>")
 })
 
-app.listen(PORT, ()=>{
+
+
+https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app).listen(PORT, ()=>{
     console.log("App is running on port ", PORT)
 })
+
