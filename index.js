d710c9500f80eb34f86fbf0a2bd110d94428f118
Reetesh Yadav <reeteshy15@gmailcom>
2024-04-03 01:28:07 +0530

    Added file upload functionality

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 import express from "express";
+import fileUpload from "express-fileupload";
+import path, {dirname } from "path";
+import { fileURLToPath } from 'url';
+
+
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const PORT = 3000;
 
-app.get('/', (req, resp) => {
-    resp.send("<h1>Welcome to home Route</h1>")
+app.set('view engine', 'ejs')
+app.use(fileUpload())
+
+app.get('/', async (req, resp) => {
+    resp.render('index')
+})
+
+app.post('/single', async (req, resp, next)=>{
+    try {
+        const file = req.files.mFile
+        console.log("file ", file)
+        const savePath = path.join(__dirname, 'public', 'upload', file.name)
+        await file.mv(savePath)
+        resp.redirect('/')
+
+    } catch (error) {
+        console.log("Error file upload : ", error)
+        resp.send("Error file upload")
+    }
 })
 
 app.listen(PORT, ()=>{
