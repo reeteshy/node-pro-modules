e2dc3bd8740d52dd8eac5825d8c184c152d5a5ee
Reetesh Yadav <reeteshy15@gmailcom>
2024-04-02 12:25:55 +0530

    added files and show like cloud

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import serveIndex from "serve-index";
+
 const app = express()
 const PORT = 3000;
 
-app.get('/', (req, resp) => {
-    resp.send("<h1>Welcome to home Route</h1>")
-})
+app.use('/ftp', express.static('public/ftp'), serveIndex('public/ftp', {'icons': true, hidden:true}))
+// app.use('/', serveIndex)
+
+// app.get('/', (req, resp) => {
+//     resp.send("<h1>Welcome to home Route</h1>")
+// })
 
 app.listen(PORT, ()=>{
     console.log("App is running on port ", PORT)
