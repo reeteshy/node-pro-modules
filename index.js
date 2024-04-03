69783adc7871f3a11dde928bbd285d208aa11422
Reetesh Yadav <reeteshy15@gmailcom>
2024-04-03 09:56:16 +0530

    added compression features

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+import compression from "compression";
 import express from "express";
 const app = express()
 const PORT = 3000;
 
+app.use(compression({
+    level:6,
+    threshold:0, //each data should be compressed
+    threshold:100 * 1000, //less then 100KB data should not be compressed
+    filter : (req, resp)=>{
+        if(req.headers['x-no-compression']){
+            return false;
+        }
+        return compression.filter(req, res)
+    }
+}))
+
 app.get('/', (req, resp) => {
     resp.send("<h1>Welcome to home Route</h1>")
+})
+
+
+app.get('/value', (req, resp) => {
+    resp.send('Welcome to home Route'.repeat(1000))
 })
 
 app.listen(PORT, ()=>{
